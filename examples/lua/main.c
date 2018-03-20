762e32ff2fc9182ab64a773740bc5dde68fef23f
danpetry <daniel.petry@fu-berlin.de>
2018-03-20 11:45:09 +0100

    examples/lua: lua cli loaded from RIOT
    have the lua CLI being compiled into RIOT and loaded up.
    Tested with a hello world example.

--- examples/lua/main.c
+++ examples/lua/main.c
@@ -22,30 +22,16 @@
 
 #include "msg.h"
 #include "shell.h"
-// #include "net/gnrc/netif.h"
-// #include "net/gnrc/pktdump.h"
-
-/* main thread's message queue */
-#define MAIN_QUEUE_SIZE     (8)
-static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-
+extern int lua_main(int argc, char **argv);
 int main(void)
 {
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-
+    
     puts("Lua RTOS build");
 
-//    /* get the default interface */
-//    gnrc_netif_t *netif;
-//
-//
-//#ifdef MODULE_NETIF
-//    gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
-//                                                          gnrc_pktdump_pid);
-//    gnrc_netreg_register(GNRC_NETTYPE_CCN_CHUNK, &dump);
-//#endif
+    char *input_string = "lua\0";
 
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    char **input_pp = &input_string;
+    lua_main(1, input_pp);
+
     return 0;
 }
