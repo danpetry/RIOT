3095c70728943c90cbc0b891ab7e1aa8c32b2bdd
danpetry <daniel.petry@fu-berlin.de>
2018-06-06 15:47:24 +0200

    added gen_and_flash.sh script

---
+++ examples/suit_updater/gen_and_flash.sh
@@ -0,0 +1,12 @@
+# usage: generate_keys_and_flash.sh <board name>
+make -C ../../dist/tools/firmware
+../../dist/tools/firmware/bin/firmware genkeys sec.key pub.key
+BOARD=$1 APP_VER=$(date +%s) make -j4 clean riotboot
+BOARD=$1 APP_VER=$(date +%s) make -j4 riotboot/flash
+
+# upload firmware and get hex
+read -p "Please rename and upload your firmware, and input the new firmware name:" firmwarename
+read -p "and now the truncated SHA1 from the riot-labs website:" firmwareSHA
+python3 gen_manifest.py -u coap://[2001:470:749d:1003::11]/f/$firmwareSHA -k ../../dist/tools/firmware/sec.key -V 1 -o manifest.cbor bin/$1/$firmwarename -r
+make term BOARD=$1
+
