244c5c454a28690cf9be70ce50e2bd89dd0f02c8
danpetry <daniel.petry@fu-berlin.de>
2018-07-09 18:04:03 +0200

    Updated IPv6 addr in script
    - So that it is the same as the server IPv6 address when running on
      laptop - i.e. the IP address of the ethernet interface once set with
      prefix 2001:db8:f4ba:cbcc::/64

--- examples/suit_updater/gen_and_flash.sh
+++ examples/suit_updater/gen_and_flash.sh
@@ -7,6 +7,6 @@ BOARD=$1 APP_VER=$(date +%s) make -j4 riotboot/flash
 # upload firmware and get hex
 read -p "Please rename and upload your firmware, and input the new firmware name:" firmwarename
 read -p "and now the truncated SHA1 from the riot-labs website:" firmwareSHA
-python3 gen_manifest.py -u coap://[fd3b:ffc5:f4ba:cbcd:7075:3587:67c1:af27]/f/$firmwareSHA -k ../../dist/tools/firmware/sec.key -V 1 -o manifest.cbor bin/$1/$firmwarename -r
+python3 gen_manifest.py -u coap://[2001:db8:f4ba:cbcc:4e17:ca27:b306:7842]/f/$firmwareSHA -k ../../dist/tools/firmware/sec.key -V 1 -o manifest.cbor bin/$1/$firmwarename -r
 make term BOARD=$1
 
